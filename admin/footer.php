7dd0b41615148692d3764c4bccca4a863ff4851c
gabelatendai <gabela.musodza33@gmail.com>
2020-03-08 11:01:36 +0400

    initial commit

---
+++ admin/footer.php
@@ -0,0 +1,60 @@
+<!-- Footer -->
+<footer class="footer">
+    <div class="row align-items-center justify-content-xl-between">
+        <div class="col-xl-6">
+            <div class="copyright text-center text-xl-left text-muted">
+                <p class="text-sm font-weight-500">Copyright 2018 © All Rights Reserved.Dashboard Template</p>
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <p class="float-right text-sm font-weight-500"><a href="www.templatespoint.net">Templates Point</a></p>
+        </div>
+    </div>
+</footer>
+<!-- Footer -->
+</div>
+</div>
+</div>
+</div>
+<!-- app-content-->
+</div>
+</div>
+<!-- Back to top -->
+<a href="#top" id="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- Ansta Scripts -->
+<!-- Core -->
+<script src="assets/plugins/jquery/dist/jquery.min.js"></script>
+<script src="assets/js/popper.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Echarts JS -->
+<script src="assets/plugins/chart-echarts/echarts.js"></script>
+
+<!-- Fullside-menu Js-->
+<script src="assets/plugins/toggle-sidebar/js/sidemenu.js"></script>
+
+<!-- Custom scroll bar Js-->
+<script src="assets/plugins/customscroll/jquery.mCustomScrollbar.concat.min.js"></script>
+
+<!-- peitychart -->
+<script src="assets/plugins/peitychart/jquery.peity.min.js"></script>
+<script src="assets/plugins/peitychart/peitychart.init.js"></script>
+
+<!-- Vector Plugin -->
+<script src="assets/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+<script src="assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="assets/plugins/jvectormap/gdp-data.js"></script>
+<script src="assets/plugins/jvectormap/jquery-jvectormap-us-aea-en.js"></script>
+<script src="assets/plugins/jvectormap/jquery-jvectormap-uk-mill-en.js"></script>
+<script src="assets/plugins/jvectormap/jquery-jvectormap-au-mill.js"></script>
+<script src="assets/plugins/jvectormap/jquery-jvectormap-ca-lcc.js"></script>
+<script src="assets/js/dashboard2map.js"></script>
+
+<!-- Ansta JS -->
+<script src="assets/js/custom.js"></script>
+<script src="assets/js/dashboard-sales.js"></script>
+
+</body>
+
+</html>
